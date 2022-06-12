15ddce2dd58fa0df68dd65c5276f1d5de00e6998
Thiago Guerrero <tgbalera@amazon.com>
2022-06-12 19:48:09 -0300

    finish mongo script + add redis in docker-compose
    Signed-off-by: Thiago Guerrero <tgbalera@amazon.com>

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
-function listToHash(list) {
-  list.sort();
-  return list.join('-');
-}
-
-const gridfs = require('gridfs');
-
 db = db.getMongo().getDB("pingr--");
 
 db.post.insertMany([
   {post_id: 1, profile_id: 1, date: Date.now(), text: {text: "Acordei bobo hoje"}},
-  {post_id: 2, profile_id: 2, date: Date.now(), text: {text: "Semana de break! #feliz", hashtags: ["#feliz"]}},
-])
+  {post_id: 2, profile_id: 2, date: Date.now(), text: {text: "Também quero dar opiniões #MonologoNão", hashtags: ["#MonologoNão"]}},
+  {post_id: 3, profile_id: 3, date: Date.now(), text: {text: "só tem um caminho certo nessas eleições #lula13 #foraBolsonaro", hashtags: ["#lula13", "#foraBolsonaro"]}},
+  {post_id: 4, profile_id: 3, date: Date.now(), text: {text: "só tem um caminho certo nessas eleições #lula13 #foraBolsonaro", hashtags: ["#lula13", "#foraBolsonaro"]}},
+]);
 
 db.chat.insertMany([
-  {chat_id: 1, account_ids: listToHash([1, 2]), messages: [
-    {account_id: 1, date: Date.now(), text: "Comi o cu de quem ta lendo"},
-    {account_id: 2, date: Date.now(), text: "Oloco, que é isso?"},
+  {chat_id: 1, account_ids: [1, 2].sort(), messages: [
+    {account_id: 1, date: Date.now(), text: "Ou mano, vc vai na qib"},
+    {account_id: 2, date: Date.now(), text: "Oloco, qq é isso?"},
   ]},
-  {chat_id: 2, account_ids: listToHash([2, 3]), messages: [
-    {account_id: 2, date: Date.now(), text: "Mano, o doido do Foo me mandou uma msg mo entranha"},
-    {account_id: 3, date: Date.now(), text: "qual?"},
-    {account_id: 2, date: Date.now(), text: "Comi o cu de quem ta lendo"},
+  {chat_id: 2, account_ids: [2, 3].sort(), messages: [
+    {account_id: 2, date: Date.now(), text: "essa semana vai ter break?"},
+    {account_id: 3, date: Date.now(), text: "vai sim"},
+    {account_id: 2, date: Date.now(), text: "top!"},
   ]},
-  {chat_id: 3, account_ids: listToHash([1, 2, 3]), owner_account_id: 1, privacy: "private", token: "#b709t", messages: [
-    {account_id: 3, date: Date.now(), text: "O Foo, para com essas porra ai irmão"},
-    {account_id: 1, date: Date.now(), text: "meu pau na sua mão"},
+  {chat_id: 3, account_ids: [1, 2, 3].sort(), owner_account_id: 1, privacy: "private", token: "#b709t", messages: [
+    {account_id: 3, date: Date.now(), text: "ou rapazeada, bora fazer o ep"},
+    {account_id: 1, date: Date.now(), text: "vamo"},
   ]},
-  {chat_id: 4, account_ids: listToHash([3]), owner_account_id: 3, privacy: "public", messages: [
-    {account_id: 3, date: Date.now(), text: "Welcome to my personal notes group!"},
+  {chat_id: 4, account_ids: [3].sort(), owner_account_id: 3, privacy: "public", messages: [
+    {account_id: 3, date: Date.now(), text: "Welcome to my personal notes group! #coursera"},
   ]},
 ]);
+
+// Queries
+
+// Recuperar todas as postagens de um usuáio específico
+db.post.find({"profile_id": 1}).pretty();
+
+// Recuperar o histórico de conversa de um chat específico
+db.chat.find({"chat_id": 2}, {_id: 0, messages: 1}).pretty();
+
+// Recuperar todas as postagens que contém uma hashtag específica
+db.post.find({"text.hashtags": {$elemMatch: { $eq: "#lula13" }}}).pretty();
+
+// Recuperar todas as postagens idênticas a uma postagem específica.
+let srcText = db.post.findOne({"post_id": 3}, {_id: 0, post_id: 1, text: "$text.text"});
+db.post.find({"post_id": {$ne: srcText.post_id}, "text.text": srcText.text}).pretty();
+
+// Recuperar todos os chats de um usuário
+db.chat.find({"account_ids": {$elemMatch: {$eq: 1}}}).pretty();
